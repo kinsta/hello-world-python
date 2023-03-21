e56cfabb87e45791989c218f500fab2474b22464
Maciek Palmowski <m.palmowski@freshpixels.pl>
2023-03-21 15:34:38 +0100

    Adds landing page

--- server.py
+++ server.py
@@ -1,16 +1,18 @@
 import os
 import http.server
+from os import curdir, sep
 import socketserver
 
 from http import HTTPStatus
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        full_file = curdir + sep + 'index.html'
+        f = open(full_file)
         self.send_response(HTTPStatus.OK)
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
-        self.wfile.write(msg.encode())
-
+        self.wfile.write(f.read().encode())
 
 port = int(os.getenv('PORT', 80))
 print('Listening on port %s' % (port))
